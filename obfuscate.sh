7b606c4048dcdc69abd31f57f144fe66dcc13cbc
cr0hn <cr0hn@cr0hn.com>
2018-01-10 15:18:37 +0100

    - fix invalid var name - fix sed rule

--- obfuscate.sh
+++ obfuscate.sh
@@ -3,7 +3,7 @@
 FILE_IN=$1
 INDEX_FILE=$2
 
-if [ -z "$ASSETS_BASE" ] || [ -z "$INDEX_FILE" ]; then
+if [ -z "FILE_IN" ] || [ -z "$INDEX_FILE" ]; then
     echo ""
     echo "script launch format: "
     echo "$0 JAVASCRIPT_FILE INDEX_FILE"
@@ -11,7 +11,7 @@ if [ -z "$ASSETS_BASE" ] || [ -z "$INDEX_FILE" ]; then
     exit 1
 fi
 
-OUT_NAME=$(echo $FILE_IN | sed 's|js|min.js|g')
+OUT_NAME=$(echo $FILE_IN | sed 's|\.js|\.min.js|g')
 
 python /usr/bin/garble.py $FILE_IN $OUT_NAME yes 2> /dev/null /dev/null
 
